5cb8097be305163c6a36249a80c495e82368ce0b
Nadiia Khoptiuk <holineinadia@gmail.com>
2023-06-05 18:06:07 +0300

    add start files

--- src/pages/consultations.js
+++ src/pages/consultations.js
@@ -1,41 +1,43 @@
+import Head from 'next/head';
 import ReactMarkdown from 'react-markdown';
 import { getConsultationsData } from '@/lib/consults';
 
-const ConsultationsPage = ({
-  individual,
-  online,
-  requirements,
-  factors,
-  // method,
-}) => {
+const ConsultationsPage = ({ individual, online, requirements, factors }) => {
   return (
-    <div>
-      <ReactMarkdown>{requirements.description}</ReactMarkdown>
-      <ReactMarkdown>{online.list}</ReactMarkdown>
-      <ReactMarkdown>{online.description}</ReactMarkdown>
+    <>
+      <Head>
+        <title>Create Next App</title>
 
-      <ul style={{ display: 'flex' }}>
-        {individual.list.map(({ id, content }) => {
-          return (
-            <li key={id}>
-              <ReactMarkdown>{content}</ReactMarkdown>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <div>
+        <ReactMarkdown>{requirements.description}</ReactMarkdown>
+        <ReactMarkdown>{online.list}</ReactMarkdown>
+        <ReactMarkdown>{online.description}</ReactMarkdown>
+
+        <ul style={{ display: 'flex' }}>
+          {individual.list.map(({ id, content }) => {
+            return (
+              <li key={id}>
+                <ReactMarkdown>{content}</ReactMarkdown>
+              </li>
+            );
+          })}
+        </ul>
+
+        <h2>CARDS</h2>
+        <ul style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+          {factors.cards.map(({ title, content }, index) => (
+            <li key={index}>
+              <h3>{title}</h3>
+              <span>
+                <ReactMarkdown>{content}</ReactMarkdown>
+              </span>
             </li>
-          );
-        })}
-      </ul>
-
-      <h2>CARDS</h2>
-      <ul style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
-        {factors.cards.map(({ title, content }, index) => (
-          <li key={index}>
-            <h3>{title}</h3>
-            <span>
-              <ReactMarkdown>{content}</ReactMarkdown>
-            </span>
-          </li>
-        ))}
-      </ul>
-    </div>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
