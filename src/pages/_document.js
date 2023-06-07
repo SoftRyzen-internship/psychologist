bc9c687bba91ffad3e5cf062ae9ae8f5d9932b34
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-06-07 09:56:29 +0300

    change attribute crossorigin to crossOrigin

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Alegreya:wght@500;800&family=Open+Sans:wght@300;400;600;700&display=swap"
           rel="stylesheet"
