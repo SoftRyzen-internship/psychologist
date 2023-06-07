f6bc213f1a320d66719b1f1e117f7a81ccc3f291
Irgey <116591589+Irgey@users.noreply.github.com>
2023-06-07 12:25:29 +0200

    added button markup, logic, styles

--- src/components/ActionButton/ActionButton.jsx
+++ src/components/ActionButton/ActionButton.jsx
@@ -1,3 +1,23 @@
-export const ActionButton = () => {
-  return <p>ActionButton</p>;
+import s from './ActionButton.module.css';
+
+export const ActionButton = ({
+  clickHandler,
+  isRenderInModal = true,
+  is404 = false,
+}) => {
+  return (
+    <button
+      className={
+        isRenderInModal
+          ? s.actionButtonModal
+          : is404
+          ? s.actionButton404
+          : s.ActionButton
+      }
+      type="button"
+      onClick={clickHandler}
+    >
+      {is404 ? 'Повернутись на головну' : 'Залишити заявку'}
+    </button>
+  );
 };
