96f36046f84a040170e55b7c7901d343af6da7a9
Irgey <116591589+Irgey@users.noreply.github.com>
2023-06-07 16:29:23 +0200

    added propTypes

--- src/components/ActionButton/ActionButton.jsx
+++ src/components/ActionButton/ActionButton.jsx
@@ -1,4 +1,7 @@
+import PropTypes from 'prop-types';
 import Link from 'next/link';
+
+import { routes } from 'routes';
 import s from './ActionButton.module.css';
 
 export const ActionButton = ({
@@ -7,17 +10,29 @@ export const ActionButton = ({
   is404 = false,
   text = 'Залишити заявку',
 }) => {
-  return is404 ? (
-    <Link className={s.actionLink404} href={'/'}>
-      {text !== 'Залишити заявку' ? text : 'Повернутись на головну'}
-    </Link>
-  ) : (
-    <button
-      className={inModalForm ? s.actionButtonModal : s.actionButton}
-      type={inModalForm ? 'submit' : 'button'}
-      onClick={clickHandler}
-    >
-      {text}
-    </button>
+  return (
+    <>
+      {is404 && (
+        <Link className={s.actionLink404} href={routes.HOME}>
+          {text !== 'Залишити заявку' ? text : 'Повернутись на головну'}
+        </Link>
+      )}
+      {!is404 && (
+        <button
+          className={inModalForm ? s.actionButtonModal : s.actionButton}
+          type={inModalForm ? 'submit' : 'button'}
+          onClick={clickHandler}
+        >
+          {text}
+        </button>
+      )}
+    </>
   );
+};
+
+ActionButton.propTypes = {
+  clickHandler: PropTypes.func,
+  inModalForm: PropTypes.bool,
+  is404: PropTypes.bool,
+  text: PropTypes.string,
 };
