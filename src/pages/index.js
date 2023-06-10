6eed4b240a7fe107ebf406f04e49416a9b6d11ca
Trehub Anatolii <tridfoto@gmail.com>
2023-06-10 23:59:24 +0300

    add fixed socials

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import { FirstView } from '@/views';
 import { getNewsList } from '@/utils/request';
 import { ActionButton } from '@/components';
+import { MainFixedSocials } from '@/views';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <main className="main">
         <FirstView />
         <ActionButton />
+        <MainFixedSocials />
       </main>
     </>
   );
