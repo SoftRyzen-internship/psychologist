80be155adbbd5eccf99f27153e4a64214809b2b4
Trehub Anatolii <tridfoto@gmail.com>
2023-06-10 22:37:09 +0300

    change Social comp, add props color, direction

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -4,10 +4,14 @@ import Youtube from 'public/icons/socyoutube.svg';
 import Instagram from 'public/icons/socinstagram.svg';
 import * as st from './Socials.module.css';
 
-export const Socials = () => {
+export const Socials = ({ color = '#2361ab', direction = 'row' }) => {
+  const iconStyle = {
+    color: color,
+    flexDirection: direction,
+  };
   return (
     <>
-      <ul className={st.list}>
+      <ul className={st.list} style={iconStyle}>
         <li className={st.item}>
           <a href="#" target="_blank" rel="noreferrer noopener">
             <Telegram className={st.imgIcon} />
