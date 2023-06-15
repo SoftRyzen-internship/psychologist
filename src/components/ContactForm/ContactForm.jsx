2b4597745699b541bd326bb3e2b7d4a5448216ff
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-06-15 23:29:17 +0200

    form data sent to telegram function, thank you component

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,31 @@
 import { Formik, Field, Form } from 'formik';
-import { ActionButton } from '..';
-import { contactValidationShema } from '../../utils/contactValidationShema';
+import axios from 'axios';
+import { useState } from 'react';
+
+import { ActionButton, ThankYou } from '..';
+import { contactValidationShema } from '@/utils/contactValidationShema';
 import AgreeSVG from 'public/icons/check-mark.svg';
 import s from './ContactForm.module.css';
 
+const TOKEN = process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN;
+const CHAT_ID = process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID;
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}`;
+
+const sendToChat = async ({ name, email, phone }) => {
+  const text = `Ім'я - ${name} Email - ${email} Телефон - ${phone} `;
+  console.log(text);
+
+  const data = await axios.post(URL_API, {
+    text,
+    parse_mod: 'HTML',
+  });
+  console.log(data);
+  return data;
+};
+
 export const ContactForm = () => {
+  const [isThankYou, setIsThankYou] = useState(false);
+
   return (
     <>
       <Formik
@@ -17,106 +38,112 @@ export const ContactForm = () => {
         validationSchema={contactValidationShema}
         onSubmit={(values, { setSubmitting }) => {
           // dispatch(onSubmit(values));
+          console.log(values);
+          sendToChat(values);
           setSubmitting(false);
+          setIsThankYou(true);
         }}
       >
         {({ errors, touched, values }) => (
           <Form className={s.form}>
-            <div>
-              <h3 className={s.title}>Записатися на прийом</h3>
-              <div className={s.fieldsWrap}>
-                <div className={s.inputWrap}>
-                  <label className={s.inputLabel} htmlFor="nameInput">
-                    Ваше ім’я
-                  </label>
-                  <Field
-                    style={
-                      touched.name &&
-                      errors.name && {
-                        outline: '1px solid var(--error-color)',
+            {!isThankYou && (
+              <div>
+                <h3 className={s.title}>Записатися на прийом</h3>
+                <div className={s.fieldsWrap}>
+                  <div className={s.inputWrap}>
+                    <label className={s.inputLabel} htmlFor="nameInput">
+                      Ваше ім’я
+                    </label>
+                    <Field
+                      style={
+                        touched.name &&
+                        errors.name && {
+                          outline: '1px solid var(--error-color)',
+                        }
                       }
-                    }
-                    className={s.inputField}
-                    type="text"
-                    name="name"
-                    id="namelInput"
-                    placeholder="Ваше ім'я"
-                  />
-                  {touched.name && errors.name ? (
-                    <div className={s.error}>{errors.name}</div>
-                  ) : null}
+                      className={s.inputField}
+                      type="text"
+                      name="name"
+                      id="namelInput"
+                      placeholder="Ваше ім'я"
+                    />
+                    {touched.name && errors.name ? (
+                      <div className={s.error}>{errors.name}</div>
+                    ) : null}
+                  </div>
+
+                  <div className={s.inputWrap}>
+                    <label className={s.inputLabel} htmlFor="emailInput">
+                      Email
+                    </label>
+                    <Field
+                      style={
+                        touched.email &&
+                        errors.email && {
+                          outline: '1px solid var(--error-color)',
+                        }
+                      }
+                      className={s.inputField}
+                      type="email"
+                      name="email"
+                      id="emailInput"
+                      placeholder="vaich@gmail.com"
+                    />
+                    {touched.email && errors.email ? (
+                      <div className={s.error}>{errors.email}</div>
+                    ) : null}
+                  </div>
+
+                  <div className={s.inputWrap}>
+                    <label className={s.inputLabel} htmlFor="phoneInput">
+                      Телефон
+                    </label>
+                    <Field
+                      style={
+                        touched.phone &&
+                        errors.phone && {
+                          outline: '1px solid var(--error-color)',
+                        }
+                      }
+                      className={s.inputField}
+                      type="phone"
+                      id="phoneInput"
+                      placeholder="+380"
+                      name="phone"
+                    />
+                    {touched.phone && errors.phone ? (
+                      <div className={s.error}>{errors.phone}</div>
+                    ) : null}
+                  </div>
                 </div>
 
-                <div className={s.inputWrap}>
-                  <label className={s.inputLabel} htmlFor="emailInput">
-                    Email
-                  </label>
+                <label className={s.checkboxLabel} htmlFor="acceptedTerms">
+                  Згоден з політикою конфеденційності
                   <Field
+                    className={s.acceptCheckbox}
+                    type="checkbox"
+                    id="acceptedTerms"
+                    name="acceptedTerms"
+                  />
+                  <span
+                    className={s.acceptSpan}
                     style={
-                      touched.email &&
-                      errors.email && {
+                      touched.acceptedTerms &&
+                      errors.acceptedTerms && {
                         outline: '1px solid var(--error-color)',
                       }
                     }
-                    className={s.inputField}
-                    type="email"
-                    name="email"
-                    id="emailInput"
-                    placeholder="vaich@gmail.com"
-                  />
-                  {touched.email && errors.email ? (
-                    <div className={s.error}>{errors.email}</div>
-                  ) : null}
-                </div>
+                  >
+                    {values.acceptedTerms && (
+                      <AgreeSVG className={s.icon} width="16" height="16" />
+                    )}
+                  </span>
+                </label>
 
-                <div className={s.inputWrap}>
-                  <label className={s.inputLabel} htmlFor="phoneInput">
-                    Телефон
-                  </label>
-                  <Field
-                    style={
-                      touched.phone &&
-                      errors.phone && {
-                        outline: '1px solid var(--error-color)',
-                      }
-                    }
-                    className={s.inputField}
-                    type="phone"
-                    id="phoneInput"
-                    placeholder="+380"
-                    name="phone"
-                  />
-                  {touched.phone && errors.phone ? (
-                    <div className={s.error}>{errors.phone}</div>
-                  ) : null}
-                </div>
+                <ActionButton inModalForm={true} is404={false} />
               </div>
-
-              <label className={s.checkboxLabel} htmlFor="acceptedTerms">
-                Згоден з політикою конфеденційності
-                <Field
-                  className={s.acceptCheckbox}
-                  type="checkbox"
-                  id="acceptedTerms"
-                  name="acceptedTerms"
-                />
-                <span
-                  className={s.acceptSpan}
-                  style={
-                    touched.acceptedTerms &&
-                    errors.acceptedTerms && {
-                      outline: '1px solid var(--error-color)',
-                    }
-                  }
-                >
-                  {values.acceptedTerms && (
-                    <AgreeSVG className={s.icon} width="16" height="16" />
-                  )}
-                </span>
-              </label>
-
-              <ActionButton inModalForm={true} is404={false} />
-            </div>
+            )}
+            {isThankYou && <ThankYou />}
           </Form>
         )}
       </Formik>
