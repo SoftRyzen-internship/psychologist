2ff3162a09109f7ddebe4edb376f0dc5b82160b5
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-06-19 17:04:37 +0300

    some cleaning 2

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,32 @@
 export * from './FirstComponent/FirstComponent';
+
+// Utils
 export * from './Container';
-export * from './ActionButton/ActionButton';
 export * from './SectionTitle/SectionTitle';
 export * from './Logo/Logo';
-export * from './MobBurger/MobBurger';
-export * from './NavBar/NavBar';
-export * from './MobileMenu/MobileMenu';
-export * from './Socials/Socials';
-export * from './FixedSocials/FixedSocials';
 export * from './OffsetImageBorder/OffsetImageBorder';
-export * from './ContactForm/ContactForm';
-export * from './Modal/Modal';
 export * from './ModalButton/ModalButton';
+export * from './NavBar/NavBar';
+export * from './ActionButton/ActionButton';
+export * from './FixedSocials/FixedSocials';
+// -------------
+
+// Consultations
 export * from './ReusableSection/ReusableSection';
+export * from './Modal/Modal';
+// -------------
+
+// See-Far-CBT
 export * from './ThankYou/ThankYou';
+export * from './ContactForm/ContactForm';
+// -------------
+
+// Home
+export * from './Socials/Socials';
+export * from './MobBurger/MobBurger';
+export * from './MobileMenu/MobileMenu';
+// -------------
+
+// Trainings
 export * from './TrainingsSection/TrainingsSection';
+// -------------
