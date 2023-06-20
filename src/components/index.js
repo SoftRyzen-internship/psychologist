2960f90b924884002f8507485c995ab1b72e5b94
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-06-20 09:38:04 +0300

    resolve conflicts

--- src/components/index.js
+++ src/components/index.js
@@ -5,14 +5,11 @@ export * from './BackToTop/BackToTop';
 export * from './SectionTitle/SectionTitle';
 export * from './Logo/Logo';
 export * from './OffsetImageBorder/OffsetImageBorder';
-<<<<<<< HEAD
 export * from './SlideButton/SlideButton';
 export * from './NewsCard/NewsCard';
 export * from './NewsText/NewsText';
 export * from './ContactForm/ContactForm';
 export * from './Modal/Modal';
-=======
->>>>>>> dbd685129d7938e34a940120978b8c1bd06b254f
 export * from './ModalButton/ModalButton';
 export * from './NavBar/NavBar';
 export * from './FixedSocials/FixedSocials';
