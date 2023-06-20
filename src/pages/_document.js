5d505b2726d03383b3d3d6b278709c6f93e1f53f
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-06-20 15:55:59 +0300

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html lang="uk">
       <Head>
-        {/* title, description, main keywords, goal, googlebots */}
-
         <meta
           name="description"
           content="Психолог, психотерапевт, тренер Юлія Сулаєва. Індивідуальні психологічні консультації, курси, майстер-класи та навчання для психологів"
@@ -18,15 +16,12 @@ export default function Document() {
           name="keywords"
           content="майстер-класи та навчання для психологів, Психолог, психотерапевт, курси, ПСИХОЛОГ, ПСИХОТЕРАПЕВТ, КУРСИ, Лікування ментальних проблем, Реабілітація після ментальних травм, Профілактика сімейної гармонії, Індивідуальні консультації із психологом, Групові консультації, Навчання методам роботи з травмами, Навчання методу SEE-FAR-CBT для психологів, Психологія лідерства, Тайм менеджмент, Курси стресостійкості, Курси ораторського мистецтва, Курси психолога Тренінг для батьків, Курси з Конфліктології, Тренінги, Тренінги для психологів, Тренер–психолог"
         />
-        <meta name="author" content="SoftRyzen intership team" />
 
-        {/* og image */}
+        <meta property="og:image" content={'/images/meta-photo.png'} />
 
-        <meta property="og:image" content={'/images/meta-julia.png'} />
+        <meta property="og:image:width" content="970" />
 
-        <meta property="og:image:width" content="458" />
-
-        <meta property="og:image:height" content="734" />
+        <meta property="og:image:height" content="500" />
 
         <meta property="og:type" content="website" />
 
@@ -35,17 +30,9 @@ export default function Document() {
           content="Юлія Сулаєва | Психолог - психотерапевт"
         />
 
-        <meta property="og:title" content="Психолог Юлія" />
-
-        {/* links for searchers */}
+        <meta property="og:title" content="Психолог Юлія Сулаєва" />
 
         <link rel="canonical" href="https://e-volunteer.vercel.app/" />
-
-        {/* <link rel="alternate" href="https://e-volunteer.vercel.app/" /> */}
-
-        {/* favicon */}
-
-        {/* <link rel="shortcut icon" href="/favicon/favicon.ico" /> */}
 
         <link
           rel="apple-touch-icon"
@@ -65,11 +52,6 @@ export default function Document() {
           href="/favicon/favicon-16x16.png"
         />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
