509e63073f4b9688d0236bc7dbe4b8b6575bf358
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-06-20 16:25:55 +0300

    proptypes

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { SpinnerMarkup } from '@/components';
 import * as styles from './Spinner.module.css';
 
@@ -18,4 +19,8 @@ export const Spinner = ({ contain }) => {
       )}
     </>
   );
+};
+
+Spinner.propTypes = {
+  contain: PropTypes.bool,
 };
