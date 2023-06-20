398c21af59967da3244c64bdac93f0d5a2437968
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-06-20 13:02:59 +0300

    color and props

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,35 +1,70 @@
-// import s from './Spinner.module.css';
-
-export const Spinner = () => {
+export const Spinner = ({ contain }) => {
   return (
     <>
-      <div className="blobs">
-        <div className="blobs-center"></div>
-        <div className="blob"></div>
-        <div className="blob"></div>
-        <div className="blob"></div>
-        <div className="blob"></div>
-        <div className="blob"></div>
-        <div className="blob"></div>
-      </div>
-      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-        <defs>
-          <filter id="goo">
-            <feGaussianBlur
-              in="SourceGraphic"
-              stdDeviation="10"
-              result="blur"
-            />
-            <feColorMatrix
-              in="blur"
-              mode="matrix"
-              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
-              result="goo"
-            />
-            <feBlend in="SourceGraphic" in2="goo" />
-          </filter>
-        </defs>
-      </svg>
+      {contain && (
+        <>
+          <div className="contain">
+            <div className="blobs">
+              <div className="blobs-center"></div>
+              <div className="blob"></div>
+              <div className="blob"></div>
+              <div className="blob"></div>
+              <div className="blob"></div>
+              <div className="blob"></div>
+              <div className="blob"></div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+              <defs>
+                <filter id="goo">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="10"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
+                    result="goo"
+                  />
+                  <feBlend in="SourceGraphic" in2="goo" />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+        </>
+      )}
+      {!contain && (
+        <>
+          <div className="blobs">
+            <div className="blobs-center"></div>
+            <div className="blob"></div>
+            <div className="blob"></div>
+            <div className="blob"></div>
+            <div className="blob"></div>
+            <div className="blob"></div>
+            <div className="blob"></div>
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <defs>
+              <filter id="goo">
+                <feGaussianBlur
+                  in="SourceGraphic"
+                  stdDeviation="10"
+                  result="blur"
+                />
+                <feColorMatrix
+                  in="blur"
+                  mode="matrix"
+                  values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
+                  result="goo"
+                />
+                <feBlend in="SourceGraphic" in2="goo" />
+              </filter>
+            </defs>
+          </svg>
+        </>
+      )}
     </>
   );
 };
