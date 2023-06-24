63fd6e434205e1a2acdf15602ce6eed0031f49a9
Nadiia Khoptiuk <holineinadia@gmail.com>
2023-06-24 22:08:30 +0300

    test preview 2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -17,11 +17,14 @@ export default function Document() {
           content="майстер-класи та навчання для психологів, Психолог, психотерапевт, курси, ПСИХОЛОГ, ПСИХОТЕРАПЕВТ, КУРСИ, Лікування ментальних проблем, Реабілітація після ментальних травм, Профілактика сімейної гармонії, Індивідуальні консультації із психологом, Групові консультації, Навчання методам роботи з травмами, Навчання методу SEE-FAR-CBT для психологів, Психологія лідерства, Тайм менеджмент, Курси стресостійкості, Курси ораторського мистецтва, Курси психолога Тренінг для батьків, Курси з Конфліктології, Тренінги, Тренінги для психологів, Тренер–психолог"
         />
 
-        <meta property="og:image" content={'/images/meta-og-preview.jpg'} />
+        <meta
+          property="og:image"
+          content={'/images/meta-og-preview-1600-1260.jpg'}
+        />
 
-        <meta property="og:image:width" content="800" />
+        <meta property="og:image:width" content="1600" />
 
-        <meta property="og:image:height" content="630" />
+        <meta property="og:image:height" content="1260" />
 
         <meta property="og:type" content="website" />
 
